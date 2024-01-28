9cafdb6b10441b9a4044a42d0a75a78de20edb46
George Higbie <georgehigbie@Georges-MBP.hsd1.il.comcast.net>
2024-01-27 20:47:09 -0600

    Add more html templates

--- store/views.py
+++ store/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    return render(request, 'store/store.html', context)
 
 def categories(request):
     all_categories = Category.objects.all()
     return {'all_categories': all_categories}
+
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'product': product}
+    return render(request, 'store/product-info.html', context)
